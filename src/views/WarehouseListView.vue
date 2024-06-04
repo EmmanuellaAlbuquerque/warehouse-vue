c321391ca6f018e7ed0a4dc694bf75dfe48aef75
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-06-03 21:50:45 -0300

    Exibe os galpões da API

--- src/views/WarehouseListView.vue
+++ src/views/WarehouseListView.vue
@@ -1,12 +1,47 @@
 <template>
   <div>
     <h1>Galpões Cadastrados</h1>
+
+    <table border="1px">
+      <thead>
+        <th>Código</th>
+        <th>Nome</th>
+        <th>Sigla</th>
+      </thead>
+
+      <tbody>
+        <tr v-for="w in warehouses" :key="w.code">
+          <td>{{ w.id }}</td>
+          <td>{{ w.name }}</td>
+          <td>{{ w.code }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'WarehouseListView'
+    name: 'WarehouseListView',
+
+    data() {
+      return {
+        warehouses: []
+      }
+    },
+    
+    async mounted() {
+      this.getWarehouses()
+    },
+
+    methods: {
+      async getWarehouses() {
+        const response = await this.$http.get('http://localhost:3000/api/v1/warehouses');
+        this.warehouses = await response.json();
+
+        return this.warehouses
+      }
+    }
   }
 </script>
 
