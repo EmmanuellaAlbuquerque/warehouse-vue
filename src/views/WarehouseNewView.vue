e145a4aa31fbdcc38952b83778d29ca607bb2947
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-06-05 11:17:23 -0300

    Adiciona cadastro de Warehouses

--- src/views/WarehouseNewView.vue
+++ src/views/WarehouseNewView.vue
@@ -1,14 +1,104 @@
 <template>
   <div>
     <h1>Cadastrar Galpão</h1>
+
+    <div class="container">
+      <div>{{ msg }}</div>
+
+      <form v-on:submit.prevent>
+        <div class="form">
+          <label>Código: </label>
+          <input type="text" v-model="form.code" placeholder="Código do galpão">
+        </div>
+
+        <div class="form">
+          <label>Nome: </label>
+          <input type="text" v-model="form.name" placeholder="Nome do galpão">
+        </div>
+
+        <div class="form">
+          <label>Endereço: </label>
+          <input type="text" v-model="form.address" placeholder="Endereço do galpão">
+        </div>
+        
+        <div class="form">
+          <label>Cidade: </label>
+          <input type="text" v-model="form.city" placeholder="Cidade do galpão">
+        </div>     
+        
+        <div class="form">
+          <label>CEP: </label>
+          <input type="text" v-model="form.cep" placeholder="CEP do galpão">
+        </div>
+        
+        <div class="form">
+          <label>Área m²: </label>
+          <input type="number" v-model="form.area" placeholder="Área do galpão">
+        </div>   
+        
+        <div class="form">
+          <label>Descrição: </label>
+          <textarea v-model="form.description" cols="30" rows="5" placeholder="Descrição do galpão"></textarea>
+        </div>
+        
+        <div class="form">
+          <button v-on:click="postWarehouse">Cadastrar</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'WarehouseNew'
+    name: 'WarehouseNew',
+
+    data() {
+      return {
+        msg: null,
+        form: {
+          name: null,
+          code: null,
+          city: null,
+          address: null,
+          cep: null,
+          area: null,
+          description: null
+        }
+      }
+    },
+
+    methods: {
+      async postWarehouse() {
+        try {
+          await this.$http.post('http://localhost:3000/api/v1/warehouses', {
+            code: this.form.code,
+            name: this.form.name,
+            code: this.form.code,
+            city: this.form.city,
+            address: this.form.address,
+            cep: this.form.cep,
+            area: this.form.area,
+            description: this.form.description
+          });
+          
+          this.msg = "Cadastrado com sucesso!";
+
+        } catch (error) {
+          this.msg = "Ops, tente novamente!";
+          console.log(error);
+        }
+      }
+    }
   }
 </script>
 
 <style>
+  .form {
+    margin-bottom: 15px;
+  }
+
+  .form input {
+    margin: 5px;
+  }
 </style>
